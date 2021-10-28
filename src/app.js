bba5a6c620732974738590fdffa4bdc5020662b1
CourtneyHoffs <courtney.thorben@gmail.com>
2021-10-29 10:40:36 +1300

    updated metric units

--- src/app.js
+++ src/app.js
@@ -51,7 +51,7 @@ function displayForecast(response) {
         width="42"
       />
       <div class="weather-forecast-temperatures">
-        <span class="weather-forecast-temperature-max"${Math.round(
+        <span class="weather-forecast-temperature-max">${Math.round(
           forecastDay.temp.max
         )}°</span>
         <span class="weather-forecast-temperature-min">${Math.round(
@@ -68,8 +68,9 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
+  console.log(coordinates);
   let apiKey = "ef674bbf770eee49c0e59275940d7a50";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
 
